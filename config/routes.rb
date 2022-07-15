dbe845df98e5d14865752908c1a304610babf575
m.mitsui <mm.techacademy@gmail.com>
2022-07-15 04:07:31 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
    root to: 'toppages#index'
    
+   #ログイン用
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   delete 'logout', to: 'sessions#destroy'
+   
    #ユザーの新規登録のURLを/signupにしたい。
    get 'signup', to: 'users#new'
    
